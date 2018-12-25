56f175c420076ba7dd7dd8958ea9585f55a0b9db
roykesh <42666643+roysh13@users.noreply.github.com>
2018-12-25 18:52:02 +0200

    fresh start - after git bullshit

---
+++ server/routes/commuteAPI.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const dummyData = require('./dummyData')
+
+
+router.get(`/map`, function (req, res) {
+    console.log("serving a map")
+    res.send(dummyData.mapData)
+})
+
+
+module.exports = router
