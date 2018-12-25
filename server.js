3b43f603387d1786274cbdf61a2883d0d0231565
roykesh <42666643+roysh13@users.noreply.github.com>
2018-12-25 19:03:32 +0200

    maybe this time will be the one

--- server.js
+++ server.js
@@ -5,11 +5,9 @@ const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
 
-
 const commuteAPI = require('./server/routes/commuteAPI')
 const musicAPI = require('./server/routes/musicAPI')
 const newsAPI = require('./server/routes/newsAPI')
-
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
@@ -18,21 +16,11 @@ app.use(bodyParser.urlencoded({
     extended: false
 }))
 
-// Mongoose setup
-// const mongoose = require('mongoose')
-// mongoose.connect('mongodb://localhost/weatherDB', {
-//     useNewUrlParser: true
-// })
-]
 app.use('/', commuteAPI)
 app.use('/', musicAPI)
 app.use('/', newsAPI)
-
-
-
 
 const port = 9000
 app.listen(port, function () {
     console.log(`Server running on ${port}`)
 })
-
