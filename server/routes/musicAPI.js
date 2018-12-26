75b8e6d3ec384c06c4011692a989782034a91e8e
roykesh <42666643+roysh13@users.noreply.github.com>
2018-12-26 21:38:56 +0200

    wow git you are a bully

--- server/routes/musicAPI.js
+++ server/routes/musicAPI.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const dummyData = require('./dummyData')
+
 const request = require(`request`)
 
 router.get(`/music`, function (req, res) {
-request(`https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyCjk-ENKPEwCWlvNmXbj604Xv-sxUttzd8&part=snippet,contentDetails,statistics,status`, (error, response, body) => {
-    let data = JSON.parse(body)
-    console.log("serving a song")
-    res.send(data)
-})
+    request(`https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyCjk-ENKPEwCWlvNmXbj604Xv-sxUttzd8&part=snippet,contentDetails,statistics,status`, (error, response, body) => {
+        let data = JSON.parse(body)
+        console.log("serving a song")
+        res.send(data)
+    })
 })
 
 module.exports = router
